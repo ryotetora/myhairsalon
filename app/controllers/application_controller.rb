378f3bc80a54bfd2fb614eba486f5513367c7043
ryotetora <surreal7295@gmail.com>
2020-12-09 02:09:37 +0900

    全てに制限はかけないよう修正

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,6 +1,5 @@
 class ApplicationController < ActionController::Base
-  before_action :authenticate_user!
-  # ログインしてなければログイン画面にとばす
+  
   before_action :configure_permitted_parameters, if: :devise_controller?
 
   private
