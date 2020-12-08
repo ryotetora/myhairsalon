f6814c87ef70fc8ce1446a5db4b28813058c997e
ryotetora <surreal7295@gmail.com>
2020-12-09 02:09:15 +0900

    仮アクション設定

--- app/controllers/tweets_controller.rb
+++ app/controllers/tweets_controller.rb
@@ -1,4 +1,12 @@
 class TweetsController < ApplicationController
+  before_action :authenticate_user!, only:[:create]
+  # ログインしてなければ投稿はできずログイン画面にとばす
   def index
+  end
+
+  def create
+  end
+
+  def show
   end
 end
