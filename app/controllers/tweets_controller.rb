334a388c77e213cce134d5ff69048deeccca9bb1
ryotetora <surreal7295@gmail.com>
2020-12-09 15:11:12 +0900

    ストパラ、保存アクション設定

--- app/controllers/tweets_controller.rb
+++ app/controllers/tweets_controller.rb
@@ -5,9 +5,27 @@ class TweetsController < ApplicationController
     @tweets = Tweet.all
   end
 
+  def new
+    @tweet = Tweet.new
+  end
+
   def create
+    @tweet = Tweet.create(tweet_params)
+   if @tweet.save
+      redirect_to root_path
+      # 保存成功ならTOPにとばす
+   else
+      render :new
+      # 失敗ならエラー文付近にとばす
+   end
   end
 
   def show
   end
+
+  private
+  def tweet_params
+    params.require(:tweet).permit(:style_name, :image, :text)
+  end
+
 end
