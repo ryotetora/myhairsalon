ba84e27a77ba690ad24dec00164b3dd10eaab277
ryotetora <surreal7295@gmail.com>
2020-12-09 15:12:00 +0900

    保存のためのルーティング設定

--- config/routes.rb
+++ config/routes.rb
@@ -3,5 +3,5 @@ Rails.application.routes.draw do
   root to: "tweets#index"
   # アクセスがあればindexにとばす
   resources :users, only: [:edit, :update]
-  resources :tweets, only: :index
+  resources :tweets, only: [:index, :new, :create]
 end
