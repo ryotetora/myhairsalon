798724a6e6b75fe47c5f21a67826573a94df66b3
ryotetora <surreal7295@gmail.com>
2021-01-19 16:04:38 +0900

    編集、更新アクション設定

--- app/controllers/tweets_controller.rb
+++ app/controllers/tweets_controller.rb
@@ -24,6 +24,22 @@ class TweetsController < ApplicationController
     @tweet = Tweet.find(params[:id])
   end
 
+
+  def edit
+    @tweet = Tweet.find(params[:id])
+  end
+
+  def update
+    tweet = Tweet.find(params[:id])
+    if tweet.update(tweet_params)
+      redirect_to tweet_path
+    # 編集保存成功すれば、詳細にとばす
+    else
+    render :edit
+    # 失敗すれば編集冒頭にとばす
+    end
+  end
+
   def destroy
     @tweet = Tweet.find(params[:id])
     if @tweet.destroy
