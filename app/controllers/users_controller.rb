60264201c01bed28b0078a6b15f4cdc79aa5c544
ryotetora <surreal7295@gmail.com>
2021-01-25 15:58:24 +0900

    マイページに表示する情報を変数化

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -10,6 +10,13 @@ class UsersController < ApplicationController
     end
   end
 
+  def show
+    @tweet = Tweet.find(params[:id])
+    @nickname = current_user.nickname
+    @tweets = current_user.tweets
+# マイページに表示したい情報を変数化
+  end
+
   private
 
   def user_params
