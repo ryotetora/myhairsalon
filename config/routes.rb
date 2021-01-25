1cf598621f4e4f11c3d5128cab84c1411775ce1a
ryotetora <surreal7295@gmail.com>
2021-01-25 15:57:57 +0900

    マイページ用のルーティング設定

--- config/routes.rb
+++ config/routes.rb
@@ -2,6 +2,6 @@ Rails.application.routes.draw do
   devise_for :users
   root to: "tweets#index"
   # アクセスがあればindexにとばす
-  resources :users, only: [:edit, :update]
+  resources :users, only: [:edit, :update, :show]
   resources :tweets, only: [:index, :new, :create, :show, :edit, :update, :destroy]
 end
