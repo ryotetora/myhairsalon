7ec0273024942dc07723d44af3f421a2762a8f95
ryotetora <surreal7295@gmail.com>
2021-01-26 23:07:23 +0900

    URL直うち対策

--- app/controllers/tweets_controller.rb
+++ app/controllers/tweets_controller.rb
@@ -1,6 +1,8 @@
 class TweetsController < ApplicationController
   before_action :authenticate_user!, only:[:create]
+  # before_action :same_user_check, only: [:edit, :destroy]
   # ログインしてなければ投稿はできずログイン画面にとばす
+
   def index
     @tweets = Tweet.all.order('created_at DESC')
     #  全レコード情報をもつインスタンス変数を生成、並びを降順指定
@@ -28,6 +30,8 @@ class TweetsController < ApplicationController
 
   def edit
     @tweet = Tweet.find(params[:id])
+    redirect_to root_path unless current_user.id == @tweet.user_id
+    # ログインユーザーと投稿者が違うならTOPにとばす
   end
 
   def update
@@ -51,5 +55,4 @@ class TweetsController < ApplicationController
   def tweet_params
     params.require(:tweet).permit(:style_name, :text, :image).merge(user_id: current_user.id)
   end
-
 end
