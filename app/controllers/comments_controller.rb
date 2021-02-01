a314c2848527d8819e56711f7a6515b98d1d636b
ryotetora <surreal7295@gmail.com>
2021-02-01 13:27:42 +0900

    スペルミスの修正

--- app/controllers/comments_controller.rb
+++ app/controllers/comments_controller.rb
@@ -1,6 +1,6 @@
 class CommentsController < ApplicationController
   def create
-    comment = Comment.create(coment_params)
+    comment = Comment.create(comment_params)
     redirect_to "/tweets/#{comment.tweet.id}"
     # コメントしたら同じ詳細画面に戻る
   end
@@ -8,4 +8,6 @@ class CommentsController < ApplicationController
   private
   def comment_params
     params.require(:comment).permit(:text).merge(user_id: current_user.id, tweet_id: params[:tweet_id])
+  end
+
 end
