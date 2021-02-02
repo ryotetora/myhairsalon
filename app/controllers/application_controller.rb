b8040b0cb8188dc67f84caa6fee20883c8eaa4d8
ryotetora <surreal7295@gmail.com>
2021-02-02 10:27:33 +0900

    deviseで使用するカラムの許可

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -3,6 +3,7 @@ class ApplicationController < ActionController::Base
 
   private
   def configure_permitted_parameters
-    devise_parameter_sanitizer.permit(:sign_up, keys: [:nickname])
+    devise_parameter_sanitizer.permit(:sign_up, keys: [:nickname, :salon_name, :like_style, :salon_hp, :prefecture, :phone])
   end
+  # デフォルトで使えないパラメータの許可
 end
