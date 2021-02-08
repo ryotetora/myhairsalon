8c705caee1d30583f74117d5b31b62a742974f02
ryotetora <surreal7295@gmail.com>
2021-02-09 02:38:23 +0900

    ログアウトルーティング修正

--- config/routes.rb
+++ config/routes.rb
@@ -2,6 +2,11 @@ Rails.application.routes.draw do
   devise_for :users
   root to: "tweets#index"
   # アクセスがあればindexにとばす
+  devise_scope :user do
+    get '/users/sign_out' => 'devise/sessions#destroy'
+  end
+  # 既存のログアウトルーティングに上書き
+
   resources :users, only: [:edit, :update, :show]
   resources :tweets do
     resources :comments, only: [:create]
