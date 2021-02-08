a35e1d8f2286a713e440f87384a0875a7d5a40a1
ryotetora <surreal7295@gmail.com>
2021-02-09 01:47:42 +0900

    db修正

--- db/migrate/20210201060230_add_columns_to_users.rb
+++ db/migrate/20210201060230_add_columns_to_users.rb
@@ -1,6 +1,6 @@
 class AddColumnsToUsers < ActiveRecord::Migration[6.0]
   def change
-    # add_column :users, :salon_name, :string, after: :encrypted_password
+    add_column :users, :salon_name, :string, after: :encrypted_password
     add_column :users, :like_style, :string, after: :salon_name
     add_column :users, :salon_hp, :string, after: :like_style
     add_column :users, :prefecture, :string, after: :salon_hp
